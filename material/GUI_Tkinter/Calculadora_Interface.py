b53d44ff7109ebe3976915fd355cd10651035b28
Lucas Pagliari <lucas.alex.pagliari@gmail.com>
2019-05-24 00:06:07 -0300

    comentário

--- material/GUI_Tkinter/Calculadora_Interface.py
+++ material/GUI_Tkinter/Calculadora_Interface.py
@@ -1,61 +1,56 @@
-from tkinter import * #importando a biblioteca tkinter e todas as suas funcionalidades
+from tkinter import *
 
-#função que soma, estou pegando o texto dela atravez da função get e atribuindo
-#ao label 'lb_result' que foi criado atravez da propriedade 'text'
-def soma():
+# Operações
+def adicao():
+    # get(): Recebe os valores dos Entry
     num1 = int(n1.get())
     num2 = int(n2.get())
+    # Altera o valor do label text para a resultado da operação
+    # através da propriedade 'text'
     lb_result['text'] = num1 + num2
 
-#função que subtrai, estou pegando o texto dela atravez da função get e atribuindo
-#ao label 'lb_result' que foi criado atravez da propriedade 'text'
 def subtracao():
     num1 = int(n1.get())
     num2 = int(n2.get())
     lb_result['text'] = num1 - num2
 
-#função que multiplica, estou pegando o texto dela atravez da função get e atribuindo
-#ao label 'lb_result' que foi criado atravez da propriedade 'text'
 def multiplicacao():
     num1 = int(n1.get())
     num2 = int(n2.get())
     lb_result['text'] = num1 * num2
 
-#função que divide, estou pegando o texto dela atravez da função get e atribuindo
-#ao label 'lb_result' que foi criado atravez da propriedade 'text'
 def divisao():
     num1 = int(n1.get())
     num2 = int(n2.get())
     lb_result['text'] = ('{:.2f}'.format(num1 / num2))
 
-#criando a janela principal atravez da classe Tk
+# Cria janela principal
 janela = Tk()
 
-#Criando uma caixa de etxto atravez do widget Entry
+# Entry: cria uma caixa de entrada
 n1 = Entry(janela)
 n1.place(x=350,y= 50)
 
-#Criando uma caixa de etxto atravez do widget Entry
 n2 = Entry(janela)
 n2.place(x=350,y=90)
 
-#Criando um botão e atribuindo ele a variavel 'btn_soma'
-btn_soma = Button(janela, width= 20, bg='blue', text='Soma',font=('arial', 15, 'bold'), command=soma)
+# Criando um botão e atribuindo ele a função adição
+btn_soma = Button(janela, width= 20, bg='blue', text='Soma',font=('arial', 15, 'bold'), command=adicao)
 btn_soma.place(x=120,y=330)
 
-#Criando um botão e atribuindo ele a variavel 'btn_sub'
+# Criando um botão e atribuindo ele a função subtração
 btn_sub = Button(janela, width= 20, bg='red', text='Subtração',font=('arial', 15, 'bold'), command=subtracao)
 btn_sub.place(x=120,y=280)
 
-#Criando um botão e atribuindo ele a variavel 'btn_mult'
+# Criando um botão e atribuindo ele a função multiplicação
 btn_mult = Button(janela, width= 20, bg='yellow',text='Multiplicação',font=('arial', 15, 'bold'), command=multiplicacao)
 btn_mult.place(x=120,y=230)
 
-#Criando um botão e atribuindo ele a variavel 'btn_divi'
+# Criando um botão e atribuindo ele a função divisão
 btn_divi = Button(janela, width= 20,bg='pink', text='Divisão',font=('arial', 15, 'bold'), command=divisao)
 btn_divi.place(x=120,y=180)
 
-#criando um Label onde será atribuido o resultado das operações
+# Label onde será atribuido o resultado das operações
 lb_result = Label(janela,text='Resultado', font=('arial', 20 , 'bold'))
 lb_result.place(x=190, y=400)
 
@@ -63,14 +58,14 @@ lb_result.place(x=190, y=400)
 lb_text = Label(janela, text='Digite um numero', font=('arial', 20 , 'bold'))
 lb_text.place(x=30,y=40)
 
-#Label criado para mostrar a mensagem 'Digite um numero'
+# Label criado para mostrar a mensagem 'Digite um numero'
 lb_text2 = Label(janela, text='Digite um numero', font=('arial', 20 , 'bold'))
 lb_text2.place(x=30,y=90)
 
-#Definindo o tamanho da janela
+# Definindo o tamanho da janela
 janela.geometry('500x500+150+150')
 
-#Mantem a janela sendo exibida
+# Mantem a janela sendo exibida
 janela.mainloop()
 
 
--- material/GUI_Tkinter/exemplo1_GUI.py
+++ material/GUI_Tkinter/exemplo1_GUI.py
@@ -1,34 +1,36 @@
-#Importando a biblioteca tkinter e todos os seus modulos
+# Importando a biblioteca tkinter e todos os seus modulos
 from tkinter import *
 
-#criando a janela principal da aplicação através de classe Tk()
-#criando uma instância da classe Tk() a atribuindo a mesma na variavel 'janela'
+# Criando a janela principal da aplicação através de classe Tk()
+# criando uma instância da classe Tk()
 janela = Tk()
 
-#criando uma função para quando o 1° botão for clicado, ele pegue o nome que foi digitado na caixa de texto através do método 'get'
-#e então substitua essa mensagem no nosso 'label' usando a propriedade 'text'
+# Criando uma função para quando o 1° botão for clicado, 
+# ele pegue o nome que foi digitado na caixa de texto através do método 'get'
+# e então substitua essa mensagem no nosso 'label' usando a propriedade 'text'
 def Muda_nome():
     lb2['text'] = caixa_texto.get()
 
-#criando uma função para quando o 2° botão for clicado ele voltar a exibir a primeira menssagem 'Seu nome aqui !'
+# criando uma função para quando o 2° botão for clicado ele voltar 
+# a exibir a primeira menssagem 'Seu nome aqui !'
 def Voltar():
     lb2['text'] = 'Seu nome aqui !'
 
-#criando um label com o widget Label, atribuindo ele a janela principal, definindo a largura como 30px,
-#definindo o seu texto e definindo a fonte do texto
+# Label, atribuindo ele a janela principal, definindo a largura como 30px,
+# definindo o seu texto e definindo a fonte do texto
 lb = Label(janela, width=30, text='Digite o seu nome', font=('arial', 20, 'bold'))
 
-#usando o gerenciador de layout 'place', nos definimos as coordenadas do nosso label na janela
-#x: distância da margem esquerda para o centro/y: distância do topo da margem para o centro
+# Gerenciador de layout 'place', nos definimos as coordenadas do nosso label na janela
+# x: distância da margem esquerda para o centro
+# y: distância do topo da margem para o centro
 lb.place(x=-65, y=70)
 
-#criando uma caixa de texto com o widget Entry, atribuindo ele a janela principal e definindo a largura como 20px
+# Entrada de texto com o widget Entry, com largura de 20px
 caixa_texto = Entry(janela, width=20)
 caixa_texto.place(x=125, y=140)
 
-#criando um botão com o widget Button, atribuindo ele a janela principal, definindo a largura como 30px,
-#definindo o seu texto, definindo a cor de fundo do botão e definindo a fonte do texto
-#o parametro command está recebendo a função que foi criada, ao pressionar o botão ele executa a ação
+# criando um botão com o widget Button, atribuindo ele a janela principal, definindo a largura como 30px,
+# Button(janela, largura, altura, texto_exibido, cor_de_fundo, fonte(fonte, tamanho, estilo) , função_do_botão)
 btn1 = Button(janela, width=20, height=2, text='Clique aqui', bg='red', font=('arial', 10, 'bold'), command=Muda_nome)
 btn1.place(x=105, y=180)
 
@@ -38,18 +40,14 @@ btn2.place(x=105, y=250)
 lb2 = Label(janela, width=30, text='Seu nome aqui !', font=('arial', 20 , 'bold'))
 lb2.place(x=-65, y=330)
 
-#aqui estamos definindo o tamanho da janela que será criada atravez do método geometry
+# Definindo tamanho e posição da janela criada
 janela.geometry('400x400+400+150')
+# Largura x Altura + Distância da margem esquerda da tela + Distância em releção ao topo da tela 
 
-#1° medida (400): Largura da janela
-#2° medida (400): Altura da janela
-#3º medida (400): Medida em pixels da janela na tela da margem esquerda com relação ao centro
-#4º medida (150): Medida em pixels da janela na tela do topo com relação ao centro
-
-#Definindo o titulo da janela através do método 'title'
+# title() : Define o titulo da janela
 janela.title('Exemplo 1')
 
-#método que faz com que a janela fique sendo exibida na tela
+# Método que faz com que a janela fique sendo exibida na tela
 janela.mainloop()
 
 
